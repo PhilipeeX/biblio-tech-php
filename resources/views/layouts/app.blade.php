ed5acf0e2e612fedc6033a0a7d8d4de6160a228a
PhilipeeX <dev.philipe@outlook.com>
2025-06-13 20:55:33 -0300

    style: adds identity to flash messages and views

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Meu Sistema</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="container mt-4">
+
+  <x-flash-messages />
+
+  @yield('content')
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
